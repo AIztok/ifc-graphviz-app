b9482bdc5b9de49247d41649800c14724611fcd6
AIztok <128750281+AIztok@users.noreply.github.com>
2024-07-08 22:11:55 +0200

    Update streamlit_ifcdot.py

--- streamlit_ifcdot.py
+++ streamlit_ifcdot.py
@@ -174,8 +174,9 @@ uploaded_file = st.file_uploader("Choose an IFC file", type=["ifc"])
 # Dropdown for example IFC files
 example_files = {
     "Streifenfundamente": "https://github.com/AIztok/ifc-graphviz-app/raw/main/Examples/Streifenfundamente.ifc",
-    "4D": "https://github.com/AIztok/ifc-graphviz-app/raw/main/Examples/4D.ifc",
-    "AC20-FZK-Haus": "https://github.com/AIztok/ifc-graphviz-app/raw/main/Examples/AC20-FZK-Haus.ifc"
+    "Halbrahmen": "https://github.com/AIztok/ifc-graphviz-app/raw/main/Examples/Halbrahmen_240704.ifc"
+    #"4D": "https://github.com/AIztok/ifc-graphviz-app/raw/main/Examples/4D.ifc",
+    #"AC20-FZK-Haus": "https://github.com/AIztok/ifc-graphviz-app/raw/main/Examples/AC20-FZK-Haus.ifc"
 }
 
 example_file_choice = st.selectbox("Or select an example IFC file", list(example_files.keys()))
